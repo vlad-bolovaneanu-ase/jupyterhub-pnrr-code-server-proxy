2ed32f12455bebebf72aeefc0bd367ab82a3fbda
Vlad Bolovaneanu <vlad.bolovaneanu@ase.ro>
2025-06-23 09:18:22 +0000

    changed script execution type

--- src/jupyter_code_server/start_proxy.sh
+++ src/jupyter_code_server/start_proxy.sh
@@ -5,6 +5,20 @@ JUPYTERHUB_USER=${2}
 JUPYTERHUB_GENERIC_USER=${3:-jovyan}
 LISTEN_PORT=${4:-8888}
 
-code-server --auth=none --port=$CODE_PORT --extensions-dir=/home/$JUPYTERHUB_GENERIC_USER/.vscode/extensions --auth=none --disable-update-check --disable-file-uploads --disable-file-downloads --ignore-last-opened &
+code-server --auth=none --port=$CODE_PORT \
+    --extensions-dir=/home/$JUPYTERHUB_GENERIC_USER/.vscode/extensions \
+    --disable-update-check --disable-file-uploads --disable-file-downloads \
+    --ignore-last-opened &
 
-exec python3 proxy_wrapper.py --port $CODE_PORT --username $JUPYTERHUB_USER --listen $LISTEN_PORT
+CS_PID=$!
+
+python3 proxy_wrapper.py --port $CODE_PORT --username $JUPYTERHUB_USER --listen $LISTEN_PORT &
+
+PROXY_PID=$!
+
+# Ctrl+C
+trap "kill $CS_PID $PROXY_PID" SIGINT SIGTERM
+
+# Wait for both to finish
+wait $CS_PID
+wait $PROXY_PID
