962cf34b9cb8a1f3cacbabf161f275abb36339a9
Vlad Bolovaneanu <vlad.bolovaneanu@ase.ro>
2025-06-23 08:29:05 +0000

    allocate free port when necessary

--- src/jupyter_code_server/__init__.py
+++ src/jupyter_code_server/__init__.py
@@ -1,5 +1,9 @@
-from shutil import which
 import os
+import socket
+import logging
+
+from contextlib import closing
+from shutil import which
 from subprocess import run
 from tempfile import mkstemp
 
@@ -11,6 +15,31 @@ def which_code_server():
     if not command:
         raise FileNotFoundError('Could not find executable code-server!')
     return command
+
+
+def setup_logger():
+    logger = logging.getLogger(__name__)
+    if len(logger.handlers) == 0:
+        formatter = logging.Formatter('%(asctime)s | %(name)s | %(levelname)s | %(message)s')
+        stream_handler = logging.StreamHandler(sys.stdout)
+        stream_handler.setFormatter(formatter)
+        logger.addHandler(stream_handler)
+        file_handler = logging.FileHandler("/tmp/code_server_proxy.log")
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+    return logger
+
+
+def find_free_port():
+    with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
+        s.bind(('', 0))
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        return s.getsockname()[1]
+    
+
+def is_port_in_use(port: int):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        return s.connect_ex(('localhost', port)) == 0
 
 
 def setup_code_server():
@@ -25,17 +54,35 @@ def setup_code_server():
             "icon_path": os.path.join(_HERE, 'icons/vscode.svg')
             }
         }
+    logger = setup_logger()
+    try:
+        code_server_port = int(os.environ.get('CODE_PORT', None))
+        if is_port_in_use(code_server_port):
+            code_server_port = find_free_port()
+            logger.info(f"Use {code_server_port} as a random available port for code-server.")
+        logger.info(f"Use {code_server_port} as the port assigned by the user for code-server.")
+    except Exception:
+        code_server_port = find_free_port()
+        logger.info(f"Use {code_server_port} as a random available port for code-server.")
 
     try:
-        code_server_port = int(os.environ.get('CODE_PORT', '13777'))
+        flask_proxy_port = int(os.environ.get('CODE_PROXY_PORT', None))
+        if is_port_in_use(flask_proxy_port):
+            flask_proxy_port = find_free_port()
+            logger.info(f"Use {flask_proxy_port} as a random available port for flask proxy.")
+        logger.info(f"Use {flask_proxy_port} as the port assigned by the user for flask proxy.")
     except Exception:
-        code_server_port = 13777
+        flask_proxy_port = find_free_port()
+        logger.info(f"Use {flask_proxy_port} as a random available port for flaks proxy.")
 
     jh_generic_user = os.environ.get('NB_USER', 'jovyan')
     jh_username = os.environ.get("JUPYTERHUB_USER", None)
 
-    proxy_command = ["/bin/bash", "start_proxy.sh", code_server_port, jh_username, jh_generic_user]
+    if jh_username is None:
+        raise ValueError(f"Expected to be provided the username in 'JUPYTERHUB_USER' env var. Available env vars: {','.join(os.environ.keys())}.")
 
+    proxy_command = ["/bin/bash", "start_proxy.sh", code_server_port, jh_username, jh_generic_user, flask_proxy_port]
+    logger.info(f"Start command: {' '.join(proxy_command)}.")
     proxy_config_dict.update({"command": proxy_command})
 
     return proxy_config_dict
