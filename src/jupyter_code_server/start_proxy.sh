9c3ae5279510850d4578326fc3bca63925d75c43
Vlad Bolovaneanu <vlad.bolovaneanu@ase.ro>
2025-06-23 07:55:11 +0000

    fixed wrong parameter to proxy_wrapper

--- src/jupyter_code_server/start_proxy.sh
+++ src/jupyter_code_server/start_proxy.sh
@@ -5,17 +5,6 @@ JUPYTERHUB_USER=${2}
 JUPYTERHUB_GENERIC_USER=${3:-jovyan}
 LISTEN_PORT=${4:-8888}
 
-code-server \
-    --auth=none \
-    --port=$CODE_PORT \
-    --extensions-dir=/home/$JUPYTERHUB_GENERIC_USER/.vscode/extensions \
-    --auth=none \
-    --disable-update-check \
-    --disable-file-uploads \
-    --disable-file-downloads \
-    --ignore-last-opened &
+code-server --auth=none --port=$CODE_PORT --extensions-dir=/home/$JUPYTERHUB_GENERIC_USER/.vscode/extensions --auth=none --disable-update-check --disable-file-uploads --disable-file-downloads --ignore-last-opened &
 
-exec python3 proxy_wrapper.py \
-    --port $CODE_PORT \
-    --prefix /user/$JUPYTERHUB_USER/vscode \
-    --listen $LISTEN_PORT
+exec python3 proxy_wrapper.py --port $CODE_PORT --username $JUPYTERHUB_USER --listen $LISTEN_PORT
